16498dd1e4eb7662b20e40e0404c72a7fd5dd58c
luizlopeslpc28 <luizalcorreia28@gmail.com>
2023-06-06 12:54:57 -0300

    ATIVIDADE GERALDAO

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import SearchComponent from './SearchComponent';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SearchComponent />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 40,
   },
 });
+
+export default App;
---
+++ SearchComponent.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, FlatList, Text } from 'react-native';
+import axios from 'axios';
+
+const SearchComponent = () => {
+  const [query, setQuery] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
+      setRepositories(response.data.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Digite um termo de pesquisa"
+        value={query}
+        onChangeText={(text) => setQuery(text)}
+      />
+      <Button title="Buscar" onPress={handleSearch} />
+      <FlatList
+        data={repositories}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Text>{item.full_name}</Text>}
+      />
+    </View>
+  );
+};
+
+export default SearchComponent;
